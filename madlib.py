b22d317d2cb4fb045a5ecc655e91c6affbb7de23
Kossy Udogu <74370317+Kossy-Augusta@users.noreply.github.com>
2021-02-26 16:20:43 +0100

    Create madlib.py

---
+++ madlib.py
@@ -0,0 +1,14 @@
+# A code to collect data and output a simple madlib
+noun=input('Enter a object noun: ')
+verb=input('Enter a verb: ')
+adjective=input('Enter a adjective: ')
+article=input('Enter an article: ')
+
+
+
+
+
+print('\ntwinkle twinkle little '+noun+',')
+print('how i wonder what you '+verb+',')
+print('up above you shine so '+adjective+',')
+print('like '+article+' diamond in the sky.')
